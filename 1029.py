a427dcc38a145b6f0f427e2d65de743f511a1ef7
pauloliriopereira <pauloliriopereira@gmail.com>
2017-02-10 17:35:45 -0200

    atualização

--- 1029.py
+++ 1029.py
@@ -1,12 +1,20 @@
-
-def fib(n):
-    if n == 1:
-        return 1
-    elif n == 0:
-        return 0
+def fib(n,num_calls):
+    if n < 2:
+        num_calls += 1
+        return n, num_calls
     else:
-        return fib(n - 1) + fib(n - 2)
+        num_calls += 1
+        return fib(n - 1) + fib(n - 2), num_calls
 
-n = int(input())
-calls = fib(n)
-print(calls)
+def numeroDeCasos(entrada):
+    for j in entradas:
+        calls, num_calls = fib(j,0)
+        print("fib(%d) = %d calls = %d" % (j, num_calls - 1, calls))
+
+nCasos = int(input())
+
+entradas = []
+for i in range(nCasos):
+    entradas.insert(i, int(input()))
+
+numeroDeCasos(nCasos)
