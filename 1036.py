84798dfc00c2cbdb6147b09b7adb574ff756888a
pauloliriopereira <pauloliriopereira@gmail.com>
2017-02-15 16:21:14 -0200

    atualização

---
+++ 1036.py
@@ -0,0 +1,17 @@
+import math
+
+entrada = input().split()
+
+a = float(entrada[0])
+b = float(entrada[1])
+c = float(entrada[2])
+
+delta = b ** 2 - 4 * a * c
+
+if delta < 0 or a == 0:
+    print("Impossivel calcular")
+else:
+    x1 = (- b + math.sqrt(delta)) / (2 * a)
+    x2 = (- b - math.sqrt(delta)) / (2 * a)
+    print("R1 = %1.5f" % x1)
+    print("R2 = %1.5f" % x2)
